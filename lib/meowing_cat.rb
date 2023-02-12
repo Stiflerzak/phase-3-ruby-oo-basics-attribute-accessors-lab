c50ca7a8820a675ab2bc2c2c20c4ae1aad1308a6
Stiflerzak <hilarymwendia97@gmail.com>
2023-02-12 14:28:15 +0300

    Done with assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,31 @@
 ## code your solution here. 
+require 'pry'
+class Cat
+    attr_accessor :name
+    
+    def meow=(meow)   
+        
+        @meow= meow        
+        end
+
+        def meow
+            @meow
+            puts "meow!"
+        end   
+    
+end
+
+cat1= Cat.new
+cat1.name= "Maru"
+
+cat1.meow
+
+
+ 
+
+
+
+
+
+
+
